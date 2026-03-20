88af1660e94af2a16f89410781865479b561309f
Gabriel <gabriel.regis2005@gmail.com>
2026-03-20 09:42:18 -0300

    Subindo arquivos do dia 20/03

---
+++ Aula 20 03/Estruturas.py
@@ -0,0 +1,23 @@
+# Estrutura de repetição
+# if (se) -> verifica se uma condição é true (verdade).Se for, ele executa o código.
+# elif (senão se) -> é usado para testar várias condições.Ele só executa se todas as condições anteriores forem falsas.
+# else (senão) -> Executa o código se a condição for false(falsa).
+
+#idade_usuario = 15
+# se o usuario for maior de 18 anos, eu vou passar a informação: você é maior de idade, senão você é menor de idade;
+#if idade_usuario >= 18:
+#    print("Você é maior de idade.")
+#else:
+#    print("Você é menor de idade")
+
+# idade = 73
+# if idade >= 18:
+#     print("Você é maior de idade")
+# elif idade >= 0 and idade < 18:
+#     print("Você é jovem de idade")
+# elif idade >= 70:
+#     print("Você é expereiente de idade.")
+# else:
+#     print("Você é menor de idade")
+
+
---
+++ README.md
@@ -0,0 +1,23 @@
+# ANOTAÇÕES DE FUNDAMENTOS DA PROGRAMAÇÃO
+
+## Tipos de dados em python
+1. string
+2. number int 
+3. number float
+4. boolean
+
+## Operadores Matemáicos - basicos
++ -> adição
+- -> subtração
+* -> multiplicação
+/ -> divisão 
+
+## Métodos em python
+1. print() -> exibe informações no terminal
+
+## Format em pyhon
+
+Estrutura de repetição
+``#f (se)`` -> verifica se uma condição é true (verdade).Se for, ele executa o código.
+``elif (senão se)`` -> é usado para testar várias condições.Ele só executa se todas as condições anteriores forem falsas.
+``else (senão)`` -> Executa o código se a condição for false(falsa).
