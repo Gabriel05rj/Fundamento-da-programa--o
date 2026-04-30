247b991c234d6a33f04086d2ec140dd4ea220860
Gabriel <gabriel.regis2005@gmail.com>
2026-04-30 08:23:29 -0300

    Avaliação 01

---
+++ Prova_01/Avaliação.py
@@ -0,0 +1,70 @@
+
+Nomes = []
+while True:
+       quantidade = input("Informe a quantidade de alunos da turma: ")  
+       if quantidade == "0":
+             break
+       
+       nome = input("Informe seu nome: ")
+       nota01 = float(input("Digite sua nota: "))
+       nota02 = float(input("Digite sua nota: "))
+       nota03 = float(input("Digite sua nota: "))
+       
+       notas = [nota01, nota02, nota03]
+       media = sum(notas) / 3
+       
+       print(f"{media:.2f}")
+       
+       print('''\n---Tabela de notas:
+             [1] Aprovado (7,0 +)
+             [2] Recuperação (5,0 - 6,9)
+             [3] Reprovado( < 5,0)
+             ---''')
+       tabela = input("Escolha uma opção: ")
+
+       situacao = "";
+      
+       if tabela == "1":
+            if media >= 7.0:
+                situacao = "Aprovado";
+                registro = f"Aprovado. {media:.1f}"
+                print("Aluno aprovado!!!")
+            else:
+                print("Nota em correção.")
+                
+       elif tabela == "2":
+                if media >= 5.0 and media < 7.0:
+                     situacao = "Recuperação";
+                     registro = f"Em Recuperação.{media:.1f}"
+                     print("Aluno em Recuperação. Estude mais.")
+                else:
+                     print("Notas em avaliação")
+                     
+       elif tabela == "3":
+                    if media < 5.0:
+                        situacao = "Reprovado";
+                        registro = f"Reprovado.{media:.1f}"
+                        print("❌Aluno reprovado ❌")
+                    else:
+                        print("Notas em avaliação")
+
+       print(f"Aluno: {nome}")
+
+       aluno = {
+        "nome": nome,
+        "nota1": nota01,
+        "nota2": nota02,
+        "nota3": nota03,
+        "media": media,
+        "situacao": situacao
+       }
+       Nomes.append(aluno) 
+
+for aluno in Nomes:
+    print("\n-----------------------")
+    print(f"Nome: {aluno['nome']}")
+    print(f"Nota 1: {aluno['nota1']:.2f}")
+    print(f"Nota 2: {aluno['nota2']:.2f}")
+    print(f"Nota 3: {aluno['nota3']:.2f}")
+    print(f"Média: {aluno['media']:.2f}")
+    print(f"situacao: {aluno['situacao']}")
